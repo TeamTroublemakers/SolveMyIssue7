02de2726a251da114b833dfc72f3fdc5056dc93b
Angela Emanuelsson <angelaemanuelsson@gmail.com>
2024-01-29 15:14:30 +0100

    Issue page, issue class

--- SolveMyIssue7/obj/Debug/net7.0/SolveMyIssue7.AssemblyInfo.cs
+++ SolveMyIssue7/obj/Debug/net7.0/SolveMyIssue7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolveMyIssue7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ba3a7f977faf84bcd80c3bb41e776b06465385")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolveMyIssue7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolveMyIssue7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
